4a15bff5e998f473075878ff51c20f2b410c1259
Max <max@Maxs-MacBook-Pro.local>
2018-11-25 00:51:06 +0700

    password length testing

--- cypress/integration/setesting/register.spec.js
+++ cypress/integration/setesting/register.spec.js
@@ -2,55 +2,80 @@ var registerValidData = {
   email: "maxsayr5@gmail.com",
   password: "0847440744",
   confirmPassword: "0847440744"
-}
+};
 var registerNotValidData = {
   email: "maxsayr5gmail.com",
   password: "0847440744z",
   confirmPassword: "0847440744"
-}
+};
 
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
+var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-describe('Register Integration Test', function() {
-  beforeEach(function(){
-    cy.visit('/src/index.html')
-    cy.get('#registerLink').click()
-  })
-  it(`is email ${registerValidData['email']} valid ?`, ()=> {
+describe("Register Integration Test", function() {
+  beforeEach(function() {
+    cy.visit("/src/index.html");
+    cy.get("#registerLink").click();
+  });
+  it(`is email ${registerValidData["email"]} valid ? (True Condition)`, () => {
+    cy.get("#email").type(registerValidData["email"]);
+    cy.get("#email")
+      .invoke("val")
+      .then(val => emailRegex.test(val))
+      .then(res => expect(res).to.equal(true));
+  });
 
-    cy.get('#email').type(registerValidData['email'])
-    cy.get('#email').invoke('val')
-                    .then(val => emailRegex.test(val))
-                    .then(res => expect(res).to.equal(true))
-  })
+  it(`is email ${
+    registerNotValidData["email"]
+  } not valid ? (False Condition)`, () => {
+    cy.get("#email").type(registerNotValidData["email"]);
+    cy.get("#email")
+      .invoke("val")
+      .then(val => emailRegex.test(val))
+      .then(res => expect(res).to.equal(false));
+  });
 
-  it(`is email ${registerNotValidData['email']} not valid ?`, ()=> {
+  it("is password and confirm password equal ? (True Condition)", () => {
+    cy.get("#password").type(registerValidData["password"]);
+    cy.get("#confirmPassword").type(registerValidData["confirmPassword"]);
+    cy.get("#password")
+      .invoke("val")
+      .then(val => {
+        cy.get("#confirmPassword")
+          .invoke("val")
+          .then(vals => expect(val).to.equal(vals));
+      });
+  });
 
-    cy.get('#email').type(registerNotValidData['email'])
-    cy.get('#email').invoke('val')
-                    .then(val => emailRegex.test(val))
-                    .then(res => expect(res).to.equal(false))
-  })
+  it("is password and confirm password not equal ? (False Condition)", () => {
+    cy.get("#password").type(registerNotValidData["password"]);
+    cy.get("#confirmPassword").type(registerNotValidData["confirmPassword"]);
+    cy.get("#password")
+      .invoke("val")
+      .then(val => {
+        cy.get("#confirmPassword")
+          .invoke("val")
+          .then(vals => expect(val).not.to.equal(vals));
+      });
+  });
 
-  it('is password and confirm password equal ?', ()=> {
+  it("Password should be at least 6 characters (True Condition)", () => {
+    cy.get("#password").type("2");
+    cy.get("#password")
+      .invoke("val")
+      .then(val => {
+        expect(val).to.have.length.gte(6);
+      });
+  });
 
-    cy.get('#password').type(registerValidData['password'])
-    cy.get('#confirmPassword').type(registerValidData['confirmPassword'])
-    cy.get('#password').invoke('val').then(val=> {
-      cy.get('#confirmPassword').invoke('val').then(vals=> expect(val).to.equal(vals))
-    })
-   
-  })
+  it("Is password less than 6 characters ? (False Condition)", () => {
+    cy.get("#password").type("55555");
+    cy.get("#password")
+      .invoke("val")
+      .then(val => {
+        expect(val).to.have.length.lessThan(6);
+      });
+  });
 
-  it('is password and confirm password not equal ?', ()=> {
-
-    cy.get('#password').type(registerNotValidData['password'])
-    cy.get('#confirmPassword').type(registerNotValidData['confirmPassword'])
-    cy.get('#password').invoke('val').then(val=> {
-      cy.get('#confirmPassword').invoke('val').then(vals=> expect(val).not.to.equal(vals))
-    })
-  })
   // it('register validation test', function() {
 
   //   let mockData = {
@@ -68,14 +93,11 @@ describe('Register Integration Test', function() {
   //       newResult = Object.keys(result).map(function(key, index) {
   //         var value = result[key];
   //         return value === 0;
-  //       });     
+  //       });
   //     })
 
   //     return Object.keys(newResult).length === 0;
   //   })
 
-    
-    
-    
   // })
-})
+});
