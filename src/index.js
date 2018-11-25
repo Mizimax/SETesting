116cb862032701f2529fcd349b9325b88467ff73
TarZz <guitar_sk555@hotmail.com>
2018-11-25 18:19:42 +0700

    finish login

---
+++ src/index.js
@@ -0,0 +1,97 @@
+const db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("userNameShow").style.display ="block";
+    document.getElementById("logoutShow").style.display ="block";
+    document.getElementById("cTeamShow").style.display ="block";
+    document.getElementById("registerLink").style.display ="none";
+    document.getElementById("loginShow").style.display ="none";
+
+    var currentUser = firebase.auth().currentUser;
+    if( currentUser != null){
+      db.collection("UserInfo").doc(currentUser.uid).get().then(function(doc){
+        document.getElementById("userNameShow").innerHTML = doc.data().Nickname;
+      });
+      
+    }
+
+  } else {
+    // No user is signed in.
+    document.getElementById("userNameShow").style.display ="none";
+    document.getElementById("logoutShow").style.display ="none";
+    document.getElementById("cTeamShow").style.display ="none";
+    document.getElementById("registerLink").style.display ="block";
+    document.getElementById("loginShow").style.display ="block";
+
+  }
+});
+
+function register(e) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  var nickname = document.getElementById("nickname").value;
+  if (password === confirmPassword) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function(docRef) {
+        db.collection("Settings")
+          .doc("UserRole")
+          .get()
+          .then(function(doc) {
+            var data = doc.data();
+            db.collection("UserInfo")
+              .doc(docRef.user.uid)
+              .set({
+                Nickname: nickname,
+                Role: data["Default"]
+              });
+          });
+        window.alert("register success");
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // ...
+      });
+  } else {
+    console.log("Password must be equal");
+  }
+}
+
+function login(){
+  var email = document.getElementById("login_email").value;
+  var password = document.getElementById("login_password").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then( function(){
+      window.alert("Login success");
+      } )
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+}
+
+function logout(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    window.alert("Logout success");
+  }).catch(function(error) {
+    // An error happened.
+  });
+  
+}
